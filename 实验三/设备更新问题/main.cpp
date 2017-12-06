3f2349fa8eb743beba8392f4eaabe59316e41384
upupming@gmail.com <upupming@gmail.com>
2017-12-06 09:51:24 +0800

    renamed:    "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/.main.cpp.swp" -> "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/.fuse_hidden000008ce00000001" 	deleted:    "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/Graph.cpp" 	deleted:    "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/Graph.h" 	deleted:    "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/Graph.out" 	modified:   "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/a.out" 	modified:   "\345\256\236\351\252\214\344\270\211/\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/main.cpp"

--- 实验三/设备更新问题/main.cpp
+++ 实验三/设备更新问题/main.cpp
@@ -15,57 +15,7 @@ void printPath(const vector<int> & path, int v){
     cout << v+1 << " ";
 }
 
-int main()
-{
-    /////////////////// input info
-//    5
-//    11 11 12 12 13
-//    5 6 8 11 18
-    int n;
-    vector<int> prices;
-    vector<int> repairCost;
-    n = 5;
-    prices = vector<int>{11, 11, 12, 12, 13};
-    repairCost = vector<int>{5, 6, 8, 11, 18};
-//    cout << "请输入年数n: " ;
-//    cin >> n;
-//    int x;
-//    cout << "请输入1-" << n << "年年初的设备价格: " << endl;
-//    for(int i=0; i<n; i++){
-//        cin >> x;
-//        prices.push_back(x);
-//    }
-//    cout << "请输入使用不同时间的设备所需的维修费用: " << endl;
-//    for(int i=0; i<n; i++){
-//        cin >> x;
-//        repairCost.push_back(x);
-//    }
-
-    ////////////////// initialize adjacency matrix
-    vector<vector<int>> matrix = vector<vector<int>>(n+1);
-    for(int i=0; i<=n; i++){
-        matrix[i] = vector<int>(n+1);
-        for(int j=0; j<=n; j++){
-            if(i==j) matrix[i][j] = 0;
-            else if(i<j){
-                int sum = prices[i];
-                for(int k=0; k<j-i; k++)
-                    sum += repairCost[k];
-                matrix[i][j] = sum;
-            }
-            else matrix[i][j] = MAX_INT;
-        }
-    }
-
-    ////////////////// print the matrix
-    cout << "构造出的邻接矩阵为: " << endl;
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=n; j++){
-            cout << setw(15) << matrix[i][j] ;
-        }
-        cout << endl;
-    }
-
+void solve(int n, vector<vector<int>> matrix){
     //////////////// Dijkstra
     vector<int> dist(n+1); // distance
     vector<bool> visited(n+1);
@@ -96,6 +46,93 @@ int main()
     cout << "购进新设备的时间为: " ;
     printPath(path, n);
     cout << endl;
+}
+
+vector<vector<int>> getMatrix(int n, const vector<int> & prices, const vector<int> & repairCost){
+    ////////////////// initialize adjacency matrix
+    vector<vector<int>> matrix = vector<vector<int>>(n+1);
+    for(int i=0; i<=n; i++){
+        matrix[i] = vector<int>(n+1);
+        for(int j=0; j<=n; j++){
+            if(i==j) matrix[i][j] = 0;
+            else if(i<j){
+                int sum = prices[i];
+                for(int k=0; k<j-i; k++)
+                    sum += repairCost[k];
+                matrix[i][j] = sum;
+            }
+            else matrix[i][j] = MAX_INT;
+        }
+    }
+
+    ////////////////// print the matrix
+    cout << "构造出的邻接矩阵为: " << endl;
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            cout << setw(15) << matrix[i][j] ;
+        }
+        cout << endl;
+    }
+
+    return matrix;
+}
+
+int main()
+{
+    /////////////////// input info
+//    5
+//    11 11 12 12 13
+//    5 6 8 11 18
+    int n;
+    vector<int> prices;
+    vector<int> repairCost;
+    n = 5;
+    prices = vector<int>{11, 11, 12, 12, 13};
+    repairCost = vector<int>{5, 6, 8, 11, 18};
+    cout << "初始样例为: " << endl;
+    cout << "n = " << n << endl;
+    cout << "prices = [ ";
+    for(int i=0; i<n; i++){
+        cout << prices[i] << " ";
+    }
+    cout << "]" << endl;
+    cout << "repairCost = [ ";
+    for(int i=0; i<n; i++){
+        cout << repairCost[i] << " ";
+    }
+    cout << "]" << endl;
+//    cout << "请输入年数n: " ;
+//    cin >> n;
+//    int x;
+//    cout << "请输入1-" << n << "年年初的设备价格: " << endl;
+//    for(int i=0; i<n; i++){
+//        cin >> x;
+//        prices.push_back(x);
+//    }
+//    cout << "请输入使用不同时间的设备所需的维修费用: " << endl;
+//    for(int i=0; i<n; i++){
+//        cin >> x;
+//        repairCost.push_back(x);
+//    }
+
+    while(true){
+        solve(n, getMatrix(n, prices, repairCost));
+
+        cout << endl << "请输入年数n: " ;
+        cin >> n;
+        if(n<=0) break;
+        int x;
+        cout << "请输入1-" << n << "年年初的设备价格: " << endl;
+        for(int i=0; i<n; i++){
+            cin >> x;
+            prices.push_back(x);
+        }
+        cout << "请输入使用不同时间的设备所需的维修费用: " << endl;
+        for(int i=0; i<n; i++){
+            cin >> x;
+            repairCost.push_back(x);
+        }
+    }
 
     return 0;
 }
