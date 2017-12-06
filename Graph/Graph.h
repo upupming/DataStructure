81298c0e04e9559e33a55d386e4fb1e8ac0b3398
upupming@gmail.com <upupming@gmail.com>
2017-12-06 08:09:53 +0800

    new file:   Graph/Graph.h 	new file:   Graph/a.out 	new file:   Graph/main.cpp 	new file:   man-g++.txt 	new file:   "\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/Screenshot from 2017-12-05 20-51-05.png" 	new file:   "\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/a.out" 	new file:   "\350\256\276\345\244\207\346\233\264\346\226\260\351\227\256\351\242\230/main.cpp"

---
+++ Graph/Graph.h
@@ -0,0 +1,200 @@
+/* Adjacent list implementation of directed graph
+ * just use a symmetric matrix
+ * Printable is a type which has operator << and >>
+ */
+
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+
+template <typename Printable>
+struct AdjList{
+	int subscript;
+	bool visited;
+	// pointers to the adjacent vertices
+	vector<int> adjacencies;
+	int indegree, outdegree;
+
+	AdjList(int sub, const vector<int>& adj = vector<int>(), int in = 0, int out = 0):subscript(sub), visited(false), adjacencies(adj), indegree(in), outdegree(out){}
+	// 1. Destructor
+	~AdjList(){}
+	// 2. Copy constructor
+	AdjList(const AdjList<Printable> & rhs){
+        subscript = rhs.subscript;
+        adjacencies = rhs.adjacencies;
+        indegree = rhs.indegree;    outdegree = rhs.outdegree;
+	}
+	// 3. Move constructor
+	AdjList(AdjList<Printable> && rhs){
+        subscript = std::move(rhs.subscript);
+        adjacencies = std::move(rhs.adjacencies);
+
+        // for integer: Using std::move just changes an lsubscript to an xsubscript, so it is eligible for use with move constructors and move assignment operators.
+        indegree = std::move(rhs.indegree);    outdegree = std::move(rhs.outdegree);
+	}
+	// 4. Copy assignment
+	AdjList & operator= (const AdjList<Printable> & rhs){
+        AdjList copy = rhs;// copy constructor
+        std::swap(*this, copy);// assume that swapping is implemented either with three moves or by swapping member by member, not by three copy assignments which will cause infinite loop
+        return *this;
+	}
+	// 5. Move assignment
+	AdjList & operator=(AdjList<Printable> && rhs){
+        subscript = std::move(rhs.subscript);
+        adjacencies = std::move(rhs.adjacencies);
+
+        // for integer: Using std::move just changes an lsubscript to an xsubscript, so it is eligible for use with move constructors and move assignment operators.
+        indegree = std::move(rhs.indegree);    outdegree = std::move(rhs.outdegree);
+	}
+
+	void addAdj(int v){
+        outdegree++;
+        adjacencies.push_back(v);
+	}
+	void addAdjs(const vector<int> & adjs){
+        outdegree += adjs.size();
+        for(auto adj : adjs){
+            adjacencies.push_back(adj);
+        }
+	}
+};
+
+template<typename Printable>
+class Graph{
+private:
+    vector<Printable> vertices; // store vertices
+    vector<AdjList<Printable>> adjLists; // adjacency lists
+
+    void DFS(AdjList<Printable> & adj_list){
+        // visit v and is adjacencies if it hasn't been visit
+        if(!adj_list.visited){
+            adj_list.visited = true;
+            cout << vertices[adj_list.subscript] << " ";
+            for(int j : adj_list.adjacencies){
+                DFS(adjLists[j]);
+            }
+        }
+    }
+    void setNotVisited(){
+        for(auto & adj_list : adjLists){
+            adj_list.visited = false;
+        }
+    }
+public:
+    // Using adjacent matrix initialization, empty graph for default
+    Graph(const vector<Printable> & vertices = vector<Printable>(),const vector<vector<int>> & adjMatrix = vector<vector<int>>()){
+        this->vertices = vertices;
+        int n = vertices.size();
+        assert((int)adjMatrix.size() == n); // row equals elements size?
+        for(int i=0; i<n; i++){
+            adjLists.push_back(AdjList<Printable>(i));
+        }
+
+        for(int i=0; i<n; i++){
+            vector<int> adjs = adjMatrix[i];
+            assert((int)adjs.size() == n); // column equals elements size?
+            for(int j=0; j<n; j++){
+                if(adjs[j]){ // if i->j is an edge
+                    adjLists[i].addAdj(j); // fill in adjacent list, auto increase outdegree
+                    adjLists[j].indegree++; // increase indegree manually
+                }
+            }
+        }
+
+        printList();
+    }
+    // Input Manually
+    Graph(){
+        int numOfVertices;
+        cout << "Number of vertices: " ;
+        cin >> numOfVertices;
+        vertices = vector<Printable>(numOfVertices); // Initialize vertices
+        adjLists = vector<AdjList<Printable>>(numOfVertices); // Initialize lists
+        for(int i=0; i<numOfVertices; i++){
+            cout << "The " << i+1 << "th AdjList is : ";
+            cin >> vertices[i].subscript; // initialize vertice
+
+            int t = numOfVertices, j;
+            cout << "And subscript of its adjacencies are(end with -1) : ";
+            while(t--){
+                cin >> j;
+                if(j == -1) break; // no more adjacencies
+                if(j>=0 && j <numOfVertices){
+                    adjLists[i].addAdj(j); // create adjacent list, auto increase outdegree
+                    adjLists[j].indegree++; // increase indegree manually
+                }
+            }
+        }
+
+        printList();
+    }
+
+    void printList(){
+        cout << endl << "The adjList of your Graph is : " << endl;
+        int m = adjLists.size();
+        for(int i=0; i<m; i++){
+            cout << vertices[adjLists[i].subscript] << " : ";
+            int n = adjLists[i].adjacencies.size();
+            for(int j=0; j<n; j++){
+                cout << vertices[adjLists[i].adjacencies[j]] << " " ;
+            }
+            cout << endl;
+        }
+    }
+
+    void DFS(){
+        setNotVisited();
+        int n = adjLists.size();
+        for(int i=0; i<n; i++)
+            DFS(adjLists[i]);
+        cout << endl;
+    }
+    void DFS_noRecur(){
+        setNotVisited();
+        stack<int> s;
+        int n = vertices.size(), sub;
+        for(int i=0; i<n; i++){
+            if(!adjLists[i].visited)
+                s.push(i);
+            while(!s.empty()){
+                sub = s.top(); s.pop();
+                adjLists[sub].visited = true;
+                cout << vertices[sub] << " " ;
+
+                for(int j : adjLists[sub].adjacencies){
+                    if(!adjLists[j].visited)
+                        s.push(j);
+                }
+            }
+        }
+        cout << endl;
+    }
+    void BFS(){
+        setNotVisited();
+        queue<int> q;
+        int sub, n = vertices.size();
+
+        for(int i=0; i<n; i++){ // maybe not connected
+            if(!adjLists[i].visited) q.push(i); // begin at v[i]
+            while(!q.empty()){
+                // v[0] dequeue and all its adjacency inqueue
+                sub = q.front(); q.pop();
+                adjLists[sub].visited = true;
+                cout << vertices[sub] << " " ;
+
+                for(int j : adjLists[sub].adjacencies){
+                    if(!adjLists[j].visited)
+                        q.push(j); // inqueue: all adjs
+                }
+            }
+        }
+        cout << endl;
+    }
+};
+#endif // GRAPH_H_INCLUDED
---
+++ 设备更新问题/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <queue>
+
+
+using namespace std;
+
+#define MAX_INT 0x7FFFFFFF
+
+void printPath(const vector<int> & path, int v){
+    if(path[v]!=-1){
+        printPath(path, path[v]);
+    }
+    cout << v+1 << " ";
+}
+
+int main()
+{
+    /////////////////// input info
+//    5
+//    11 11 12 12 13
+//    5 6 8 11 18
+    int n;
+    vector<int> prices;
+    vector<int> repairCost;
+    n = 5;
+    prices = vector<int>{11, 11, 12, 12, 13};
+    repairCost = vector<int>{5, 6, 8, 11, 18};
+//    cout << "请输入年数n: " ;
+//    cin >> n;
+//    int x;
+//    cout << "请输入1-" << n << "年年初的设备价格: " << endl;
+//    for(int i=0; i<n; i++){
+//        cin >> x;
+//        prices.push_back(x);
+//    }
+//    cout << "请输入使用不同时间的设备所需的维修费用: " << endl;
+//    for(int i=0; i<n; i++){
+//        cin >> x;
+//        repairCost.push_back(x);
+//    }
+
+    ////////////////// initialize adjacency matrix
+    vector<vector<int>> matrix = vector<vector<int>>(n+1);
+    for(int i=0; i<=n; i++){
+        matrix[i] = vector<int>(n+1);
+        for(int j=0; j<=n; j++){
+            if(i==j) matrix[i][j] = 0;
+            else if(i<j){
+                int sum = prices[i];
+                for(int k=0; k<j-i; k++)
+                    sum += repairCost[k];
+                matrix[i][j] = sum;
+            }
+            else matrix[i][j] = MAX_INT;
+        }
+    }
+
+    ////////////////// print the matrix
+    cout << "构造出的邻接矩阵为: " << endl;
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            cout << setw(15) << matrix[i][j] ;
+        }
+        cout << endl;
+    }
+
+    //////////////// Dijkstra
+    vector<int> dist(n+1); // distance
+    vector<bool> visited(n+1);
+    vector<int> path(n+1); // precedence
+    queue<int> q;
+
+    for(int i=0; i<=n; i++){
+        dist[i] = MAX_INT;
+        visited[i] = false;
+    }
+    dist[0] = 0; path[0] = -1;
+    q.push(0);
+
+    while(!q.empty()){
+        int v = q.front(); q.pop();
+        visited[v] = true;
+
+        for(int j=0; j<=n; j++){
+            if(!visited[j] && matrix[v][j] < dist[j]-dist[v]){
+                dist[j] = dist[v]+matrix[v][j];
+                path[j] = v;
+                q.push(j);
+            }
+        }
+    }
+
+    cout << "最短时间为: " << dist[n] << endl;
+    cout << "购进新设备的时间为: " ;
+    printPath(path, n);
+    cout << endl;
+
+    return 0;
+}
