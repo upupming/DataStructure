74ba38abf22a106df30572fc9479647deb4cf524
upupming@gmail.com <upupming@gmail.com>
2018-01-06 10:51:40 +0800

    注释judgeBST

--- review/code/judgeBST.cpp
+++ review/code/judgeBST.cpp
@@ -5,11 +5,16 @@ struct Node{
 typedef struct Node *BST;
 
 bool judgeBST(const BST t){
-    if(t->lchild && t->rchild) 
-        return (t->lchild->data < t->data && t->data < t->rchild->data) && judgeBST(t->lchild) && judgeBST(t->rchild);
-    else if(t->lchild)
-        return (t->lchild->data < t->data) && judgeBST(t->lchild);
-    else if(t->rchild)
-        return (t->data < t->rchild->data) && judgeBST(t->rchild);
+    if(t->lchild && t->rchild) // 有左右子树
+        return (t->lchild->data < t->data 
+                && t->data < t->rchild->data) 
+               && judgeBST(t->lchild) 
+               && judgeBST(t->rchild);
+    else if(t->lchild) // 只有左子树
+        return (t->lchild->data < t->data) 
+               && judgeBST(t->lchild);
+    else if(t->rchild) // 只有右子树
+        return (t->data < t->rchild->data) 
+               && judgeBST(t->rchild);
 }
 
